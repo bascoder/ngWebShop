6d81e5a8ab8d7d330c75da418b32168d5545356a
Bas van Marwijk <12039179@student.hhs.nl>
2015-09-02 21:26:29 +0200

    Update e2e tests

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -2,41 +2,55 @@
 
 /* https://github.com/angular/protractor/blob/master/docs/toc.md */
 
-describe('my app', function() {
+describe('my app', function () {
 
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
+  it('should automatically redirect to / when location hash/fragment is empty/invalid', function () {
     browser.get('index.html');
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
+    expect(browser.getLocationAbsUrl()).toMatch('/');
+
+    browser.get('index.html#/invalid');
+    expect(browser.getLocationAbsUrl()).toMatch('/');
   });
 
 
-  describe('view1', function() {
+  describe('home', function () {
 
-    beforeEach(function() {
-      browser.get('index.html#/view1');
+    beforeEach(function () {
+      browser.get('index.html#/');
     });
 
 
-    it('should render view1 when user navigates to /view1', function() {
+    it('should render home when user navigates to /', function () {
       expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
+        toMatch(/This web shop is full of AngularJS directives, services, controllers and filters./);
     });
 
   });
 
 
-  describe('view2', function() {
+  describe('about', function () {
 
-    beforeEach(function() {
-      browser.get('index.html#/view2');
+    beforeEach(function () {
+      browser.get('index.html#/about');
     });
 
 
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
+    it('should render about when user navigates to /about', function () {
+      expect(element.all(by.css('[ng-view] h1')).first().getText()).
+        toMatch(/About this web shop/);
     });
 
+  });
+
+  describe('shop', function () {
+    beforeEach(function () {
+      browser.get('index.html#/shop');
+    });
+
+    it('should render shop when user navigates to /shop', function () {
+      expect(element.all(by.css('[ng-view] label')).first().getText())
+        .toMatch(/Filter/);
+    });
   });
 });
