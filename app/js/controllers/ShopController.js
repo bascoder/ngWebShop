fdabdee06ae8a4579ce076f8b3eafd1762f0b820
Bas van Marwijk <12039179@student.hhs.nl>
2015-09-02 20:32:53 +0200

    Add shop page, it shows products from products.json file

--- app/js/controllers/ShopController.js
+++ app/js/controllers/ShopController.js
@@ -3,9 +3,39 @@
 
   var app = angular.module('shopApp');
 
-  app.controller('ShopController', [ShopController]);
+  app.controller('ShopController', ['$scope', '$http', '$log', ShopController]);
 
-  function ShopController() {
+  function ShopController($scope, $http, $log) {
+    $scope.products = [exampleProduct];
+    $scope.errorMessage = false;
 
+    $scope.humanizeDuration = function (duration) {
+      return moment.duration(-duration, 'months').humanize();
+    };
+
+    $http.get('api/products.json').then(function (response) {
+      $log.debug('Got success response', response);
+
+      var data = response.data;
+      if (angular.isArray(data)) {
+        $scope.products = data;
+      }
+    }, function (error) {
+      $log.error(error);
+      $scope.errorMessage = error['data'] || 'something went wrong, review the output log';
+    });
+
+    var exampleProduct = {
+      "brand": "Placeholder",
+      "founder": {
+        "last": "van Marwijk",
+        "first": "Bas"
+      },
+      "color": "brown",
+      "age": 84,
+      "picture": "http://lorempixel.com/100/100/business",
+      "balance": "$0.00",
+      "id": 0
+    };
   }
 })();
