ea8ab877d5f374cdb6a0cc32d599485dfb180c37
Bas van Marwijk <12039179@student.hhs.nl>
2015-09-02 20:55:16 +0200

    Show loading sign while fetching products and simulate server load by using timeout

--- app/js/controllers/ShopController.js
+++ app/js/controllers/ShopController.js
@@ -3,39 +3,51 @@
 
   var app = angular.module('shopApp');
 
-  app.controller('ShopController', ['$scope', '$http', '$log', ShopController]);
+  app.controller('ShopController',
+    ['$scope', '$http', '$log', '$rootScope', '$timeout', ShopController]);
 
-  function ShopController($scope, $http, $log) {
-    $scope.products = [exampleProduct];
+  function ShopController($scope, $http, $log, $rootScope, $timeout) {
+    var placeholderProduct = {
+      "brand": "Placeholder",
+      "founder": {
+        "last": "Rivera",
+        "first": "Rose"
+      },
+      "color": "green",
+      "age": 20,
+      "picture": "http://lorempixel.com/100/100/sports?_=147",
+      "balance": "1530.4",
+      "id": 47
+    };
+
+    $scope.products = [placeholderProduct];
     $scope.errorMessage = false;
 
     $scope.humanizeDuration = function (duration) {
       return moment.duration(-duration, 'months').humanize();
     };
 
-    $http.get('api/products.json').then(function (response) {
-      $log.debug('Got success response', response);
+    // start fetching products
+    function fetchProducts() {
+      $rootScope.isLoading = true;
 
-      var data = response.data;
-      if (angular.isArray(data)) {
-        $scope.products = data;
-      }
-    }, function (error) {
-      $log.error(error);
-      $scope.errorMessage = error['data'] || 'something went wrong, review the output log';
-    });
+      // simulate loading delay
+      $timeout(function () {
+        $http.get('api/products.json').then(function (response) {
+          $log.debug('Got success response', response);
+          var data = response.data;
+          if (angular.isArray(data)) {
+            $scope.products = data;
+          }
+        }, function (error) {
+          $log.error(error);
+          $scope.errorMessage = error['data'] || 'something went wrong, review the output log';
+        }).finally(function () {
+          $rootScope.isLoading = false;
+        });
+      }, 2000);
+    }
 
-    var exampleProduct = {
-      "brand": "Placeholder",
-      "founder": {
-        "last": "van Marwijk",
-        "first": "Bas"
-      },
-      "color": "brown",
-      "age": 84,
-      "picture": "http://lorempixel.com/100/100/business",
-      "balance": "$0.00",
-      "id": 0
-    };
+    fetchProducts();
   }
 })();
